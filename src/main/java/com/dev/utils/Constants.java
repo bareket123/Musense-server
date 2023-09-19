e239304bf7c9add6f2c740b076814614306c589e
barek <bareketatia@gmail.com>
2023-09-20 02:46:00 +0300

    delete unUsed things

--- src/main/java/com/dev/utils/Constants.java
+++ src/main/java/com/dev/utils/Constants.java
@@ -2,9 +2,5 @@ package com.dev.utils;
 
 public class Constants {
     public static final int MINIMAL_PASSWORD_LENGTH = 6;
-    public static final int SECOND = 1000;
-    public static final int MINUTE = 60 * SECOND;
-    public static final String EVENT_FOLLOWING = "1";
-
 
 }
